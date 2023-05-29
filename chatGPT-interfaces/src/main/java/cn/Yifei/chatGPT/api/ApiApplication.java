6b517e9798b07565226fa3f47c4e75eac89ffbdc
Yifei Zeng <yuz557#psu.edu>
2023-05-29 12:45:07 -0400

    Build Basic DDD structure.

---
+++ chatGPT-interfaces/src/main/java/cn/Yifei/chatGPT/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package cn.Yifei.chatGPT.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yifei
+ * @description Main Entry
+ * @date 2023/5/29
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatGPT-interfaces/src/test/java/cn/Yifei/chatGPT/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package cn.Yifei.chatGPT.api.test;
+
+/**
+ * @author Yifei
+ * @description Unit Test
+ * @date 2023/5/29
+ */
+
+public class ApiTest {
+}
